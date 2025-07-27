b29dd7bebe638713ce023bec0add9963a98e15e6
FizxyNotsDev <fikiaditya144@gmail.com>
2025-07-27 08:54:00 +0700

    Update tiktok.js

--- src/api/download/tiktok.js
+++ src/api/download/tiktok.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import cheerio from "cheerio"
+import * as cheerio from "cheerio"; // ⬅️ ESM style for latest Cheerio
 import qs from "qs"
 
 async function tikDownloader(urlss) {
